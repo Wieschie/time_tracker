b882288db45ea8db045ec306bb19e480ad31f6d7
Wieschie <supernova190@gmail.com>
2016-08-23 14:56:21 -0500

    Fix basic argument parsing.

--- time_tracker.py
+++ time_tracker.py
@@ -3,11 +3,11 @@ import logged_event
 
 parser = argparse.ArgumentParser(description='Track time spent on your computer.')
 
-# assume a task is starting if no flag provided
-parser.add_argument('-f', dest='finish', default=False, help='finish a task')
-
 # get activity type
-parser.add_argument('activity', choices=['g', 't', 'w'], default='g', nargs=1, help='type of activity', metavar='activity')
+parser.add_argument('activity', choices=['g', 't', 'w'], default='g', help='type of activity', metavar='activity')
+
+# assume a task is starting if no flag provided
+parser.add_argument('-f', dest='finish', default=False, action='store_true', help='finish a task')
 
 flags = parser.parse_args()
 
