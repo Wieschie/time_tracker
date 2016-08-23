7ff29b4c404a4d42295ba490b6788aebbbbb9d39
Wieschie <supernova190@gmail.com>
2016-08-23 17:52:47 -0500

    Implement basic append to test Google Sheet.

--- time_tracker.py
+++ time_tracker.py
@@ -1,18 +1,23 @@
 import argparse
 import logged_event
+from sheets_upload import upload
 
-# takes an event and records it
-def record_event(e):
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+# takes an event and records it in a google sheet
+def record_event_sheets(e):
     print(e.__dict__)
-
-
+    upload(e.time, e.event_type)
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Track time spent on your computer.')
 
     # get activity type
-    parser.add_argument('activity', choices=['g', 't', 'w', 'f'], default='g',
-                        help='type of activity: gaming, tv, work, or finish (end tracking)', metavar='activity')
+    parser.add_argument('activity', choices=['g', 't', 'w', 'f'], default='g', help='type of activity: gaming, tv, work, or finish (end tracking)', metavar='activity')
     
     # assume a task is starting if no flag provided
     # parser.add_argument('-f', dest='finish', default=False, action='store_true', help='finish a task')
@@ -21,6 +26,6 @@ if __name__ == '__main__':
     
     e = logged_event.event(flags.activity)
 
-    record_event(e)
+    record_event_sheets(e)
     
 
