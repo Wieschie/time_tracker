6d13072095ffd066e7c6faf39e25ae235a3126da
Wieschie <evan.schiewe@gmail.com>
2016-08-23 21:37:28 -0500

    Add consts.py for ease of maintenance.

---
+++ consts.py
@@ -0,0 +1,6 @@
+# actual time sheet
+ID = '1abkU6C-H3pUg6CNdNTEHYnXXwfsSE7wPScJs0N2PpJc'
+
+RANGE = 'experimental!A:B'
+
+APP_NAME = 'python_time_tracker'
--- sheets_upload.py
+++ sheets_upload.py
@@ -1,32 +1,33 @@
-# from __future__ import print_function
 import httplib2
 import os
 import json
-from collections import OrderedDict 
+from collections import OrderedDict
 
 from apiclient import discovery
 import oauth2client
 from oauth2client import client
 from oauth2client import tools
 
+import consts
 
 def upload(time, event_type):
+    """ uploads a single event to google sheets
+
+    """
+    
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
                     'version=v4')
     service = discovery.build('sheets', 'v4', http=http,
                               discoveryServiceUrl=discoveryUrl)
-    # test sheet
-    id = '1PDMqvbnSSwGrXdkeSqAVa9eMgJWZ2VwiulxnTSEZt9Y'
-    r='Sheet1!A:B'
-
+    
     ValueRange = {}
-    ValueRange["range"]=r
+    ValueRange["range"]=consts.RANGE
     ValueRange["majorDimension"]="ROWS"
     ValueRange["values"]=[[time, event_type]]
     result = service.spreadsheets().values().append(
-        spreadsheetId=id, range=r, valueInputOption='USER_ENTERED', body=ValueRange).execute()
+        spreadsheetId=consts.ID, range=consts.RANGE, valueInputOption='USER_ENTERED', body=ValueRange).execute()
     print(result)
     
 def get_credentials():
@@ -41,7 +42,7 @@ def get_credentials():
 
     SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
     CLIENT_SECRET_FILE = 'client_secret.json'
-    APPLICATION_NAME = 'python_time_tracker'
+    APPLICATION_NAME = consts.APP_NAME
     
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
@@ -56,13 +57,6 @@ def get_credentials():
         raise Exception("Credentials have not been set up on this machine.")
     elif credentials.invalid:
         raise Exception("Credentials are invalid.")
-#        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-#        flow.user_agent = APPLICATION_NAME
-#        if flags:
-#            credentials = tools.run_flow(flow, store, flags)
-#        else: # Needed only for compatibility with Python 2.6
-#        credentials = tools.run(flow, store)
-#        print('Storing credentials to ' + credential_path)
     return credentials
 
 
