b8d38091edb2787a39b24db1250d11de17cc2f11
Wieschie <evan.schiewe@gmail.com>
2016-08-23 22:27:19 -0500

    Remove dead code.

--- time_tracker.py
+++ time_tracker.py
@@ -18,9 +18,6 @@ if __name__ == '__main__':
     # get activity type
     parser.add_argument('activity', choices=['g', 't', 'w', 'f'], default='g', help='type of activity: gaming, tv, work, or finish (end tracking)', metavar='activity')
     
-    # assume a task is starting if no flag provided
-    # parser.add_argument('-f', dest='finish', default=False, action='store_true', help='finish a task')
-    
     flags = parser.parse_args()
     
     e = logged_event.event(flags.activity)
