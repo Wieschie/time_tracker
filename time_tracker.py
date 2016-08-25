04bb83994f1963e033ca538085ae2f3c960232a5
Wieschie <evan.schiewe@gmail.com>
2016-08-25 13:39:49 -0500

    Add testing flag.

--- time_tracker.py
+++ time_tracker.py
@@ -27,18 +27,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Track time spent on your computer.')
 
     # get activity type
-    parser.add_argument('-activity', choices=['g', 't', 'w', 'p', 'f'], default='g',
+    parser.add_argument('-activity', default='g',
                         help='type of activity: gaming, tv, work, or finish (end tracking)', metavar='activity')
 
     # allow user to manually specify time
     parser.add_argument('-t', dest='time', required=False, action=TimeAction,
                         default=datetime.utcnow().replace(microsecond=0).isoformat(),
                         help="Manually specify a start time in 'HHMM' format")
-    
+
+    parser.add_argument('--test', dest='test', action='store_true', default=False)
+
     flags = parser.parse_args()
     
     e = logged_event.Event(flags.activity, flags.time)
 
     print(e)
-    # record_event_sheets(e)
-    # record_event_local(str(e))
+    if not flags.test:
+        record_event_sheets(e)
+        record_event_local(str(e))
