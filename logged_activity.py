fb1fc8f7c69b66730e58084c97e73b2529d7dd93
Wieschie <supernova190@gmail.com>
2016-08-26 17:03:09 -0500

    Clarify variable names.

--- logged_activity.py
+++ logged_activity.py
@@ -4,8 +4,8 @@ from datetime import datetime, timedelta
 
 class Activity(Event):
 
-    def __init__(self, dt_begin: datetime, atype: str, dt_end: datetime):
-        super(Activity, self).__init__(dt_begin, atype)
+    def __init__(self, dt_begin: datetime, activity_type: str, dt_end: datetime):
+        super(Activity, self).__init__(dt_begin, activity_type)
         self.dt_end = dt_end
 
     def __str__(self):
--- logged_event.py
+++ logged_event.py
@@ -3,13 +3,13 @@ from datetime import datetime
 
 class Event(object):
     
-    def __init__(self, dt: datetime, atype: str):
+    def __init__(self, dt: datetime, activity_type: str):
         self.dt_begin = dt
-        self.activity_type = atype
+        self.activity_type = activity_type
 
     def __str__(self):
         return self.get_datetime() + ", " + self.activity_type
 
-    # returns datetime as an iso formatted string
+    # returns datetime as an iso formatted string.  Microseconds just aren't important to me.
     def get_datetime(self) -> str:
         return self.dt_begin.replace(microsecond=0).isoformat()
