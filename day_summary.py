c20a5a8dba01bf5693e0bf0251f4bc1c917137c2
Wieschie <supernova190@gmail.com>
2016-09-04 13:15:49 -0500

    Fix bug in combining activities.

--- day_summary.py
+++ day_summary.py
@@ -5,13 +5,13 @@ from consts import DT_ZERO
 
 
 class Day(object):
-    """ custom data type to store information on a day in the life of the user """
+    """ custom data type that handles totalling a day's tracked time and printed formatting """
     def __init__(self):
         self.activities = defaultdict(lambda: timedelta())
         self.total_td = DT_ZERO
 
     def add_activity(self, activity: str, time: timedelta):
-        self.activities[activity] = time
+        self.activities[activity] += time
         self.total_td += time
 
     def __str__(self):
