a7df3a1dfdf1de5ec4b1f2c2dfc43a106664deed
Wieschie <supernova190@gmail.com>
2016-09-05 12:37:23 -0500

    Add support for specifying a log file on the command line.

--- analysis.py
+++ analysis.py
@@ -76,7 +76,8 @@ if __name__ == '__main__':
     # TODO parse some args
     parser = argparse.ArgumentParser(description='Analyze time.log.')
     parser.add_argument('-n', dest='last_n_days', help="Display a summary of the last n days.", type=int, default=None)
+    parser.add_argument('-f', dest='filename', help="Specify a log file to analyze.", type=str, default="time.log")
     flags = parser.parse_args()
-    with open("time.log", 'r') as log:
+    with open(flags.filename, 'r') as log:
         activity_list = parse_time_log(log)
     print_day_summary(activity_list, flags.last_n_days)
